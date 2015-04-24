14c8d2f8a75fec594e3e5d432904f3b32a5c238d
Yanxing Wang <demiwangya@gmail.com>
2015-04-24 16:58:27 -0700

    mini server

---
+++ Mini_Server/src/core/GameServer.java
@@ -0,0 +1,180 @@
+package core;
+
+// Java Imports
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+
+// Other Imports
+import config.GameServerConf;
+import metadata.Constants;
+import metadata.GameRequestTable;
+import util.ConfFileParser;
+import util.ConfigureException;
+import util.Log;
+
+/**
+ * The GameServer class serves as the main module that runs the server. Incoming
+ * connection requests are established and redirected to be managed by another
+ * class called the GameClient. Several specialized methods are also stored here
+ * to perform other specific needs.
+ */
+public class GameServer {
+
+    // Singleton Instance
+    private static GameServer server;
+    // Configuration Variables
+    private final int port;
+    private final int num_threads;
+    // Objects
+    private final ServerSocket serverSocket;
+    private final List<ClientHandler> clientHandlerThreads = Collections.synchronizedList(new ArrayList<ClientHandler>());
+    // Lookup Tables
+    private final Map<String, GameClient> activeClients = new HashMap<>(); // Session ID -> Client
+    // Other
+    private boolean isActive = true; // Server Loop Flag
+
+    /**
+     * Create the GameServer by setting up the request types and creating a
+     * connection with the database.
+     *
+     * @param port
+     * @param num_threads
+     * @throws IOException
+     */
+    public GameServer(int port, int num_threads) throws IOException {
+        this.port = port;
+        this.num_threads = num_threads;
+
+        serverSocket = new ServerSocket(port);
+    }
+
+    public static GameServer getInstance() {
+        return server;
+    }
+
+    /**
+     * Configure tables.
+     * @throws ConfigureException
+     */
+    public void configure() throws ConfigureException {
+        // Initialize tables for global use
+        GameRequestTable.init(); // Contains request codes and classes
+    }
+
+    /**
+     * Run the game server by waiting for incoming connection requests.
+     * Establishes each connection and stores it into a GameClient to manage
+     * incoming and outgoing activity.
+     */
+    private void run() {
+        Log.consoleln("Now accepting connections...");
+        // Loop indefinitely to establish multiple connections
+        while (isActive) {
+            try {
+                // Accept the incoming connection from client
+                Socket clientSocket = serverSocket.accept();
+                Log.printf("%s is connecting...", clientSocket.getInetAddress().getHostAddress());
+                // "Random" ID
+                String session_id = UUID.randomUUID().toString();
+                // Create a runnable instance to represent a client that holds the client socket
+                GameClient client = new GameClient(session_id, clientSocket);
+                activeClients.put(client.getID(), client);
+                // Keep track of the new client thread
+                if (clientHandlerThreads.size() > num_threads) {
+                    Collections.sort(clientHandlerThreads, ClientHandler.SizeComparator);
+                    clientHandlerThreads.get(0).add(client);
+                } else {
+                    ClientHandler handler = new ClientHandler(client);
+                    handler.start();
+
+                    clientHandlerThreads.add(handler);
+                }
+            } catch (IOException ex) {
+                Log.println_e(ex.getMessage());
+            }
+        }
+    }
+
+    public void shutdown() {
+        synchronized (this) {
+            isActive = false;
+
+            for (GameClient client : activeClients.values()) {
+                client.end();
+            }
+        }
+    }
+
+    public int getPort() {
+        return port;
+    }
+
+    public int getNumThreads() {
+        return num_threads;
+    }
+
+    public void removeClientHandler(ClientHandler handler) {
+        synchronized (clientHandlerThreads) {
+            clientHandlerThreads.remove(handler);
+        }
+    }
+
+    public GameClient getActiveClient(String session_id) {
+        return activeClients.get(session_id);
+    }
+
+    public void setActiveClient(GameClient client) {
+        activeClients.put(client.getID(), client);
+    }
+
+    public List<GameClient> getActiveClients() {
+        return new ArrayList<>(activeClients.values());
+    }
+
+    public void removeActiveClient(String session_id) {
+        activeClients.remove(session_id);
+    }
+
+    public boolean hasClient(String session_id) {
+        return activeClients.containsKey(session_id);
+    }
+
+    /**
+     * Initiates the Game Server by configuring and running it. Restarts
+     * whenever it crashes.
+     *
+     * @param args contains additional launching parameters
+     */
+    public static void main(String[] args) {
+        Log.printf("Mini Server v%s is starting...", Constants.CLIENT_VERSION);
+
+        try {
+            Log.console("Loading Configuration File...");
+            GameServerConf config = new GameServerConf(new ConfFileParser("conf/gameServer.conf").parse());
+            Log.println("Done!");
+
+            server = new GameServer(config.getPortNumber(), Constants.MAX_CLIENT_THREADS);
+            server.configure();
+            server.run();
+        } catch (IOException ex) {            
+            Log.printf_e(ex.getMessage());
+            if (server != null) {
+                Log.printf_e("Port %d is in use", server.getPort());
+            }
+        } catch (ConfigureException ex) {
+            Log.printf_e(ex.getMessage());
+        } catch (Exception ex) {
+            Log.println_e("Server Crashed!");
+            Log.println_e(ex.getMessage());
+        }
+        
+        System.exit(0);
+    }
+}
